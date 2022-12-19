d9efda6e418672a236e56cc1c16f7f2527d2fd53
Weiqifan1 <cmlykke@hotmail.com>
2022-12-19 20:56:41 +0100

    2022-12-19_2056_testNewEndpoint

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import render_template
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-
+api = Api(app)
 
 @app.route("/")
 def hello_world():
     # return "hello owrld"
     return render_template("index.html")
+
+class Video(Resource):
+    def newapi(self):
+        # return "hello owrld"
+        return ""
+
+
+
+api.add_resource(Video, '/newapi')
