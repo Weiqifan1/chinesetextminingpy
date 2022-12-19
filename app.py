9773466e4f4b2785e780d716fdc180a5fa785592
Weiqifan1 <cmlykke@hotmail.com>
2022-12-19 21:25:17 +0100

    2022-12-19_2125_testNewEndpoint

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 def hello_world():
     # return "hello owrld"
     return render_template("index.html")
+
+@app.route("/newapi")
+def hello_2():
+    # return "hello owrld"
+    return render_template("index.html")
