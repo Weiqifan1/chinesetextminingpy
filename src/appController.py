e81aa21ad1ad81aeaa4f066367b3c928178c739f
Weiqifan1 <cmlykke@hotmail.com>
2022-12-28 16:32:01 +0100

    2022-12-28_1629_removedClassFromChineseParser

--- src/appController.py
+++ src/appController.py
@@ -1,21 +1,27 @@
 import json
 from chinese import ChineseAnalyzer
 import re
-from src.classes.ChineseParser import ChineseParser
+
+from src.classes import ChineseParser
+
+
+#from src.classes.ChineseParser import ChineseParser
+
+
 
 def postendpoint(postinput):
     gettextdata = postinput["text"]
     betterdata = re.sub("\s+", " ", gettextdata.strip())
-    chineseParser = ChineseParser()
-    sentences = textToTokens(betterdata, chineseParser)
+    ChineseParser.initChineseParser()
+    sentences = textToTokens(betterdata)
     return sentences
 
 def sentenceToDict(sen):
     return sen
 
-def textToTokens(text, chineseParser):
-    test2 = chineseParser.getSentences(text)
-    result = [senToDict(x, chineseParser) for x in test2]
+def textToTokens(text):
+    test2 = ChineseParser.getSentences(text)
+    result = [senToDict(x) for x in test2]
     return result
 
 def isCharChinese(singleChar):
@@ -35,36 +41,36 @@ def isChinese(firstElem):
     else:
         return False
 
-def findSubtokenPairs(firstElem, remainElems, listOfCharsToPinyinTupples, chineseParser):
-    pinyin = chineseParser.getPinyinStringFromWord(firstElem)
+def findSubtokenPairs(firstElem, remainElems, listOfCharsToPinyinTupples):
+    pinyin = ChineseParser.getPinyinStringFromWord(firstElem)
     pinyinSplit = pinyin.split()
-    test = chineseParser.getTokensFromSentence(firstElem)
+    test = ChineseParser.getTokensFromSentence(firstElem)
 
     if len(firstElem) == 0 and len(remainElems) == 0:
         return listOfCharsToPinyinTupples
     elif len(firstElem) == 0:
-        return findSubtokenPairs(remainElems, firstElem, listOfCharsToPinyinTupples, chineseParser)
+        return findSubtokenPairs(remainElems, firstElem, listOfCharsToPinyinTupples)
     elif len(pinyinSplit) > 1:
         lenOfSplit = len(test[0])
         shrinkFirstElemAgain = firstElem[:(lenOfSplit - len(firstElem))]
         lastSection = firstElem[(lenOfSplit - len(firstElem)):]
-        return findSubtokenPairs(shrinkFirstElemAgain, lastSection + remainElems, listOfCharsToPinyinTupples, chineseParser)
+        return findSubtokenPairs(shrinkFirstElemAgain, lastSection + remainElems, listOfCharsToPinyinTupples)
     elif len(pinyin) > 0 and pinyin != firstElem:
         listOfCharsToPinyinTupples.append((firstElem, pinyin))
-        return findSubtokenPairs(remainElems, "", listOfCharsToPinyinTupples, chineseParser)
+        return findSubtokenPairs(remainElems, "", listOfCharsToPinyinTupples)
     elif len(firstElem) == 1:
         listOfCharsToPinyinTupples.append((firstElem, firstElem))
-        return findSubtokenPairs(remainElems, "", listOfCharsToPinyinTupples, chineseParser)
+        return findSubtokenPairs(remainElems, "", listOfCharsToPinyinTupples)
     else:
         shrinkFirstElem = firstElem[:-1]
         lastChar = firstElem[-1]
-        return findSubtokenPairs(shrinkFirstElem, lastChar + remainElems, listOfCharsToPinyinTupples, chineseParser)
+        return findSubtokenPairs(shrinkFirstElem, lastChar + remainElems, listOfCharsToPinyinTupples)
 
-def cleanedToken(tokenPinyinTuple, chineseParser):
+def cleanedToken(tokenPinyinTuple):
     firstElem = tokenPinyinTuple[0]
     secondElem = tokenPinyinTuple[1]
     if firstElem == secondElem and isChinese(firstElem):
-        return findSubtokenPairs(firstElem, "", [], chineseParser)
+        return findSubtokenPairs(firstElem, "", [])
     else:
         return tokenPinyinTuple
 
@@ -80,19 +86,19 @@ def flattenNestedList(cleanedMergedList, outputList):
         rest = cleanedMergedList[1:]
         return flattenNestedList(rest, newList)
 
-def generatePinyinTokenPairs(sent, chineseParser):
-    basicTokens = ' '.join(chineseParser.getTokenListFromSentence(sent)).split()
-    basicPinyin = chineseParser.getPinyinStringFromWord(sent).split()
+def generatePinyinTokenPairs(sent):
+    basicTokens = ' '.join(ChineseParser.getTokenListFromSentence(sent)).split()
+    basicPinyin = ChineseParser.getPinyinStringFromWord(sent).split()
     mergedLists = list(zip(basicTokens, basicPinyin))
-    cleanedMergedList = [cleanedToken(x, chineseParser) for x in mergedLists]
+    cleanedMergedList = [cleanedToken(x) for x in mergedLists]
     result = flattenNestedList(cleanedMergedList, [])
     return result
 
-def senToDict(sent, chineseParser):
-    pinyinTokens = generatePinyinTokenPairs(sent, chineseParser)
+def senToDict(sent):
+    pinyinTokens = generatePinyinTokenPairs(sent)
     mydict = {
         "sentence": sent,
-        "rawTokens": chineseParser.getTokenListFromSentence(sent),
+        "rawTokens": ChineseParser.getTokenListFromSentence(sent),
         "wordToPinyinTuples": pinyinTokens
     }
     return mydict
--- src/classes/ChineseParser.py
+++ src/classes/ChineseParser.py
@@ -1,29 +1,29 @@
 from chinese import ChineseAnalyzer
 
-class ChineseParser:
-    def __init__(self):
-        self.chineseLib = ChineseAnalyzer()
+def initChineseParser():
+    global parser
+    parser = ChineseAnalyzer()
 
-    def getSentences(self, inputTest):
-        anal = self.chineseLib.parse(inputTest)
-        sent = anal.sentences()
-        return sent
+def getSentences(inputTest):
+    anal = parser.parse(inputTest)
+    sent = anal.sentences()
+    return sent
 
-    def getTokensFromSentence(self, sentence):
-        tokenList = ' '.join(self.chineseLib.parse(sentence).tokens()).split()
-        return tokenList
+def getTokensFromSentence(sentence):
+    tokenList = ' '.join(parser.parse(sentence).tokens()).split()
+    return tokenList
 
-    def getTokenListFromSentence(self, sentence):
-        res = self.chineseLib.parse(sentence).tokens()
-        return res
+def getTokenListFromSentence(sentence):
+    res = parser.parse(sentence).tokens()
+    return res
 
-    def getPinyinStringFromWord(self, text):
-        pinyinStr = self.chineseLib.parse(text).pinyin()
-        return pinyinStr
+def getPinyinStringFromWord(text):
+    pinyinStr = parser.parse(text).pinyin()
+    return pinyinStr
 
-    def getPinyinFromSentence(self, sentence):
-        basicPinyin = self.chineseLib.parse(sentence).pinyin().split()
-        return basicPinyin
+def getPinyinFromSentence(sentence):
+    basicPinyin = parser.parse(sentence).pinyin().split()
+    return basicPinyin
 
 
 
